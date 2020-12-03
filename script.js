2a8da4b1f06f257b5694674492fb4fa54233c737
Riyad <59559190+xLEADERx@users.noreply.github.com>
2020-12-04 00:21:36 +0530

    added random text and bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-var text = 'occas sorted eved feat the wer life and humblame that the lay with bennet anxiouse patibut eps scarospect you name charlour';
+var ParaArray = ['occas sorted eved feat the wer life and humblame that the lay with bennet anxiouse patibut eps scarospect you name charlour',
+            "Books contribute to every ones' lives in one way or another. The impact left on us by books can begin from",
+            "uplifting our mood to making us proficient in a certain field. The books are a medium through which writers",
+            "There are many kinds of books based on their size, packaging, and genres. An individual can read any book",
+            "to gain knowledge about that particular field. Reading is an activity that is not bound for any particular age",
+            "But it is suggested that children should be encouraged to read more books for it later becomes a good practice",
+            "For general knowledge about anything, we seek books like encyclopedias, atlas, and dictionaries",
+            "there are also those books that guide us while we are dealing with certain tasks like cookbooks, travel",
+            "It is no news that books are a great source of entertainment as there are many kinds of readers",
+            "fan of books like novels, poetry books, short stories, magazines, comics, etc. Sometimes reading also",
+            "becomes an addiction, but in my opinion, it is a good kind of addiction that will only be beneficial for an",
+            "the first printing press published book was developed by Johannes Gutenberg in Germany",
+            "incident set a different mark in the history of humankind's development. From then to now, there have been",
+            "Among all the information acquired from books, we tend to remember those which are vital and real to us",
+            "The most beautiful things in the world cannot be seen or touched, they are felt with the heart.",
+            "Why, sometimes I've believed as many as six impossible things before breakfast",
+            "Just remember that the things you put into your head are there forever, he said. You might want to think about that",
+            "You can tell yourself that you would be willing to lose everything you have in order to get something you want",
+            "The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time",
+            "He allowed himself to be swayed by his conviction that human beings are not born once and for all on the day their mothers give birth to them",
+            "There is an idea of a Patrick Bateman, some kind of abstraction, but there is no real me, only an entity, something illusory"];
+var text = "";
 var i = 0;
 var active = false;
 var t2 = document.getElementById("t2");
@@ -22,13 +43,17 @@ function check(x, letter) {
     if (x == letter) {
         if (i == 0) {
             st_time = Math.floor(Date.now() / 1000);
-        } else if (i == text.length - 1) {
+        } 
+        else if (i == text.length - 1) {
             curr_time = Math.floor(Date.now() / 1000);
-            setWPM()
+            setWPM();
+            getword();
+            //reset cursor 
+            i = -1;
         }
         i++;
         t2.innerHTML = text;
-        highlight(i)
+        highlight(i);
         dupli = false;
     } else if (dupli == false) {
         errors++;
@@ -52,15 +77,18 @@ function checkfocus() {
 }
 
 function getword() {
+    var index = Math.floor(Math.random() * ParaArray.length);
+    text = ParaArray[index]
     t2.innerHTML = text;
 }
+
 function setWPM(secs) {
     var sec = curr_time - st_time;
     var min = sec / 60;
     var words = text.split(' ').length;
-    var wpa = words / min;
+    var wpa = Math.round(words / min);
     var wp = document.getElementById('wpa');
-    wp.innerHTML = Math.round(wpa)
+    wp.innerHTML = wpa;
 
 
     //ERRORS
